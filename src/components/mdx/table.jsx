5a9e18d25df0fbcfe4cce7c73789752d1a7bd5b4
oscar.xie <oscar87xie@gmail.com>
2021-03-25 22:50:13 +0800

    ✨ mdx table component

---
+++ src/components/mdx/table.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { Box, useColorMode } from '@chakra-ui/react';
+import styled from '@emotion/styled';
+
+const StyledTable = styled.table`
+  width: 100%;
+  margin: 2rem 0;
+  border: 1px solid;
+  border-color: inherit;
+  th, td {
+    padding: .5rem 1rem;
+    font-size: .875rem;
+  }
+  th {
+    white-space: nowrap;
+    font-weight: 600;
+    background: ${(props) => props.colorMode === 'dark' ? 'rgba(255, 255, 255, 0.06)' : '#F7FAFC'};
+  }
+`;
+
+const Table = ({ children }) => {
+  const { colorMode } = useColorMode();
+  return (
+    <Box overflowX="scroll">
+      <StyledTable colorMode={colorMode}>
+        {children}
+      </StyledTable>
+    </Box>
+  );
+};
+
+export default Table;
