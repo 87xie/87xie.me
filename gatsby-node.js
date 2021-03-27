8bc31fc2a281f65d03b9190bfb43c7416125e2d4
oscar.xie <oscar87xie@gmail.com>
2021-03-28 02:00:43 +0800

    ✨ create post list pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -36,4 +36,21 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
       },
     });
   });
+
+  // post list
+  const perPage = 6;
+  const totalPages = Math.ceil(posts.length / perPage);
+
+  Array.from({ length: totalPages }).forEach((_, i) => {
+    actions.createPage({
+      path: i === 0 ? '/posts' : `/posts/${i + 1}`,
+      component: require.resolve('./src/templates/post-list-template.jsx'),
+      context: {
+        limit: perPage,
+        skip: i * perPage,
+        currentPage: i + 1,
+        totalPages,
+      },
+    });
+  });
 };
