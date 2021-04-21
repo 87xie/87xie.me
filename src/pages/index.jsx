279ca9643f6469802d8061affbdfb944f6778b70
oscar.xie <oscar87xie@gmail.com>
2021-04-22 00:44:31 +0800

    ✨ init pages

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import { Box } from '@chakra-ui/react';
+import LottieContainer from '@components/lottie-container';
+import underMaintenance from '../assets/animations/under-maintenance.json';
 
 const Index = () => (
-  <Box>Index Page</Box>
+  <Box
+    display="flex"
+    justifyContent="center"
+    alignItems="center"
+    height="100%"
+  >
+    <LottieContainer
+      tip="Work In Progress"
+      animationData={underMaintenance}
+    />
+  </Box>
 );
 
 export default Index;
