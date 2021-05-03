2eaf66e0763367d1536f757d19349c1bcd48dffa
oscar.xie <oscar87xie@gmail.com>
2021-05-03 19:27:49 +0800

    ✨ all tags page

--- src/pages/tags.jsx
+++ src/pages/tags.jsx
@@ -1,24 +1,58 @@
 import React from 'react';
-import { Box } from '@chakra-ui/react';
+import PropTypes from 'prop-types';
+import { graphql, Link } from 'gatsby';
+import { Badge, Stack } from '@chakra-ui/react';
 import SEO from '@components/seo/seo';
-import LottieContainer from '@components/lottie-container';
-import dancingPallbearers from '../assets/animations/dancing-pallbearers.json';
 
-const Index = () => (
-  <>
-    <SEO title="tags" />
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      height="100%"
-    >
-      <LottieContainer
-        tip="Work In Progress"
-        animationData={dancingPallbearers}
-      />
-    </Box>
-  </>
-);
+const TagsPage = ({ data }) => {
+  const tags = data?.allMdx?.group || [];
 
-export default Index;
+  return (
+    <>
+      <SEO title="tags" />
+      <Stack
+        spacing="4"
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        height="100%"
+      >
+        {tags.map(({ fieldValue: tag }) => (
+          <Link key={tag} to={`/tag/${tag}`}>
+            <Badge
+              px="2"
+              fontSize="md"
+              colorScheme="red"
+            >
+              {tag}
+            </Badge>
+          </Link>
+        ))}
+      </Stack>
+    </>
+  );
+};
+
+TagsPage.propTypes = {
+  data: PropTypes.shape({
+    allMdx: PropTypes.shape({
+      group: PropTypes.arrayOf(PropTypes.shape({
+        fieldValue: PropTypes.string.isRequired,
+        totalCount: PropTypes.number.isRequired,
+      })),
+    }),
+  }),
+};
+
+export const query = graphql`
+  query {
+    allMdx {
+      group(field: frontmatter___tags) {
+        fieldValue
+        totalCount
+      }
+    }
+  }
+`;
+
+export default TagsPage;
