0336d05e9ae794f5b6e174629c4ba9467f1c4365
oscar.xie <oscar87xie@gmail.com>
2021-05-20 00:54:19 +0800

    🏡 update chakra-ui gatsby plugin settings

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,14 +1,5 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
-import theme from './src/styles/theme';
 import DefaultLayout from './src/layouts/default/default-layout';
-
-export const wrapRootElement = ({ element }) => (
-  <ChakraProvider theme={theme}>
-    {element}
-  </ChakraProvider>
-);
 
 export const wrapPageElement = ({ element, props }) => (
   <DefaultLayout {...props}>
