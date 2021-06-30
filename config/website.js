34e5f2c7209cc16a4dd01bf9eba823cf7eab023a
oscar.xie <oscar87xie@gmail.com>
2021-07-01 01:04:11 +0800

    🏡 update site title

--- config/website.js
+++ config/website.js
@@ -1,6 +1,6 @@
 exports.siteMetadata = {
   author: 'oscar87xie',
-  title: '87xie',
+  title: '87xie.me',
   description: 'Personal writing for improving my memory.',
   url: 'https://87xie.me',
   image: 'https://87xie.me/images/patrick-star-in-love.jpg',
