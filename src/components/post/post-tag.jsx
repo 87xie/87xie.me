802726929855e1f23c08e5f646861840682c82bf
oscar.xie <oscar87xie@gmail.com>
2021-07-20 16:08:58 +0800

    💅 make tag component composable

---
+++ src/components/post/post-tag.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  Tag,
+  Link as ChakraLink,
+} from '@chakra-ui/react';
+import {
+  Link as GatsbyLink,
+} from 'gatsby';
+import PropTypes from 'prop-types';
+
+const PostTag = ({
+  to,
+  size = 'md',
+  children,
+  ...props
+}) => (
+  <ChakraLink as={GatsbyLink} to={to} {...props}>
+    <Tag size={size} colorScheme="red">
+      {children}
+    </Tag>
+  </ChakraLink>
+);
+
+PostTag.propTypes = {
+  to: PropTypes.string.isRequired,
+  size: PropTypes.string,
+  children: PropTypes.node,
+};
+
+export default PostTag;
