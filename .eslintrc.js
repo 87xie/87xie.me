00b391f534e39f3030785efa41b45c969f28742b
87xie <oscar87xie@gmail.com>
2022-02-27 21:35:16 +0800

    🚨 update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,12 +41,13 @@ module.exports = {
   rules: {
     'react/jsx-filename-extension': [0],
     'import/prefer-default-export': 'off',
-    'arrow-body-style': ['error', 'as-needed'],
+    'arrow-body-style': 0,
     'react/require-default-props': 0,
     'react/no-array-index-key': 0,
     'no-confusing-arrow': 0,
     'react/react-in-jsx-scope': 0,
     'react/jsx-props-no-spreading': 0,
     'react/function-component-definition': 0,
+    'react/prop-types': [2, { skipUndeclared: true }],
   },
 };
