d4921c781195d9eba4c19fb2a6604258477a9405
87xie <oscar87xie@gmail.com>
2024-02-19 22:14:45 +0800

    feat: migrate to astro

--- __mocks__/use-debounce.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  useDebouncedCallback: jest.fn((fn) => fn),
-};
--- jest-preprocess.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-const babelOptions = {
-  presets: [
-    'babel-preset-gatsby',
-    '@babel/preset-typescript'
-  ],
-};
-
-module.exports = require('babel-jest').default.createTransformer(babelOptions);
