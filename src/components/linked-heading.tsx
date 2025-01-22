6d3269e17a42fb14d740b1152971f61d0b825a27
87xie <oscar87xie@gmail.com>
2025-01-22 21:53:49 +0800

    feat: add aria-label to permalink for accessibility

--- src/components/linked-heading.tsx
+++ src/components/linked-heading.tsx
@@ -16,18 +16,22 @@ function LinkedHeading({
   children,
 }: LinkedHeadingProps) {
   const Heading = `h${level}` as const
+  const linkAriaLabel = typeof children === 'string'
+    ? `Permalink: ${children}`
+    : undefined
 
   return (
     <Heading
       className={cx(
         'group relative',
         level === 1 && 'text-3xl',
-        level <= 2 && 'border-b-1 border-gray-200/90 pb-1',
+        level <= 2 && 'border-b-1 border-gray-200 pb-1',
       )}
     >
       <a
         id={id}
         href={`#${id}`}
+        aria-label={linkAriaLabel}
         className={cx(
           'inline-flex absolute top-[calc(50%-0.5rem)] -left-6',
           'opacity-0 group-hover:opacity-100 focus:opacity-100',
