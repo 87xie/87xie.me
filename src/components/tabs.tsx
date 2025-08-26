4dd31556f72c8c586528b86bdcd10ba761c5e67c
87xie <oscar87xie@gmail.com>
2025-08-26 23:37:59 +0800

    feat: simplify tabs api

--- src/components/tabs.tsx
+++ src/components/tabs.tsx
@@ -1,54 +1,63 @@
-import type {
-  TabsRootProps as ArkTabsRootProps,
-  TabListProps as ArkTabListProps,
-  TabContentProps as ArkTabContentProps,
-} from '@ark-ui/react'
+'use client'
+import type { ReactElement, ReactNode } from 'react'
+import { Children, isValidElement, useMemo } from 'react'
+import type { TabsRootProps as ArkTabsRootProps } from '@ark-ui/react'
 import { Tabs as ArkTabs } from '@ark-ui/react/tabs'
-import cx from 'clsx'
+import cn from 'clsx'
 
-export function Tabs(props: ArkTabsRootProps) {
+export function Tabs({
+  children,
+  defaultValue: defaultValueProp,
+}: ArkTabsRootProps) {
+  const labels = useMemo(() => {
+    const filtered = Children.toArray(children).filter(hasLabelProp)
+    return filtered.map(({ props }) => props.label)
+  }, [children])
+  const defaultValue = defaultValueProp ?? labels?.[0] ?? ''
+
   return (
-    <ArkTabs.Root {...props} />
+    <ArkTabs.Root defaultValue={defaultValue}>
+      <ArkTabs.List
+        className={cn(
+          'relative mb-6',
+          'shadow-[0_-1px_0_0_inset_var(--color-gray-300)]',
+        )}
+      >
+        {labels.map((label) => (
+          <ArkTabs.Trigger
+            key={label}
+            value={label}
+            className={cn(
+              'min-w-10 py-2 px-4 cursor-pointer text-gray-500 font-medium',
+              'data-selected:text-(--tw-prose-body)',
+            )}
+          >
+            {label}
+          </ArkTabs.Trigger>
+        ))}
+        <ArkTabs.Indicator className="absolute w-[var(--width)] h-[2px] bottom-0 bg-gray-500" />
+      </ArkTabs.List>
+      {children}
+    </ArkTabs.Root>
   )
 }
 
-type TabListProps = {
-  items: string[]
-} & ArkTabListProps
+type TabItemProps = {
+  label: string
+  children: ReactNode
+}
 
-export function TabList({
-  items,
-  className,
-  ...arkTabListProps
-}: TabListProps) {
+export function TabItem({ label, children }: TabItemProps) {
   return (
-    <ArkTabs.List
-      {...arkTabListProps}
-      className={cx(
-        className,
-        'relative mb-6',
-        'shadow-[0_-1px_0_0_inset_var(--color-gray-300)]',
-      )}
-    >
-      {items.map((item) => (
-        <ArkTabs.Trigger
-          key={item}
-          value={item}
-          className={cx(
-            'min-w-10 py-2 px-4 cursor-pointer text-gray-500',
-            'data-selected:text-(--tw-prose-body)',
-          )}
-        >
-          {item}
-        </ArkTabs.Trigger>
-      ))}
-      <ArkTabs.Indicator className="absolute w-[var(--width)] h-[2px] bottom-0 bg-gray-500" />
-    </ArkTabs.List>
+    <ArkTabs.Content value={label}>
+      {children}
+    </ArkTabs.Content>
   )
 }
 
-export function TabContent(props: ArkTabContentProps) {
-  return (
-    <ArkTabs.Content {...props} />
-  )
+function hasLabelProp(element: ReactNode): element is ReactElement<TabItemProps> {
+  const isValid = isValidElement(element)
+  if (!isValid) return false
+  const props = element.props as TabItemProps
+  return typeof props?.label === 'string'
 }
